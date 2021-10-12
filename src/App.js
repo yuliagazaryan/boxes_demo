7a37d264c308d27f073ce529601b56369906cea3
Margit Tennosaar <margit.tennosaar@bc.fi>
2021-10-12 13:54:25 +0300

    added props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import "./App.css";
 
+const Box = (props) => {
+  return (
+    <div className="box">
+      <div>
+        <h2>{props.name}</h2>
+        <p>Title: {props.title}</p>
+        <p>Age: {props.age}</p>
+      </div>
+    </div>
+  );
+};
+
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-      <h1>Hello World</h1>
-    </>
+    <div>
+      <header>
+        <h1>Logo</h1>
+      </header>
+      <main>
+        <Box name="Maria" title="CEO" age="29" />
+        <Box name="Kati" title="Designer" age="25" />
+        <Box name="Karin" title="Developer" age="45" />
+      </main>
+      <footer>
+        <p>
+          <small>Copyrights</small>
+        </p>
+      </footer>
+    </div>
   );
 }
 

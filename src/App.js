8884c06006377b362aa34c98e2a9941f156e0696
Margit Tennosaar <margit.tennosaar@bc.fi>
2021-10-12 14:29:58 +0300

    separated components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import "./App.css";
-
-const Box = (props) => {
-  return (
-    <div className="box">
-      <div>
-        <h2>{props.name}</h2>
-        <p>Title: {props.title}</p>
-        <p>Age: {props.age}</p>
-      </div>
-    </div>
-  );
-};
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Main";
 
 function App() {
   return (
     <div>
-      <header>
-        <h1>Logo</h1>
-      </header>
-      <main>
-        <Box name="Maria" title="CEO" age="29" />
-        <Box name="Kati" title="Designer" age="25" />
-        <Box name="Karin" title="Developer" age="45" />
-      </main>
-      <footer>
-        <p>
-          <small>Copyrights</small>
-        </p>
-      </footer>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import Box from "./components/Box";
+
+const Main = () => {
+  return (
+    <main>
+      <Box name="Maria" title="CEO" age="29" />
+      <Box name="Kati" title="Designer" age="25" />
+      <Box name="Karin" title="Developer" age="45" />
+    </main>
+  );
+};
+
+export default Main;
